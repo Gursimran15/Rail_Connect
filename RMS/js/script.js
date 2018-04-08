b1faa46601750a854f4cbe5091ad09da45c8882f
Gursimran15 <ishleen892@gmail.com>
2018-04-08 06:52:26 +0530

    Add files via upload

---
+++ RMS/js/script.js
@@ -0,0 +1,10 @@
+var typed3 = new Typed('.Status', {
+    strings: ['Rail_Connect-The Railway Management System.', 'Live Status.Ticket Booking.Train Enquiry.', 'All at one Place.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1500,
+    cursorChar:'',
+     // this is a default
+    loop: true
+  });
+ 
